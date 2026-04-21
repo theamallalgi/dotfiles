0d1f9093bdf86d5dc28894a129a1863224c73f7f
Amal Lalgi <amallalgi2487@gmail.com>
2026-04-21 15:50:53 +0530

    refactor: upgrade to modern syntax

--- .config/nvim/lua/amal/core/settings.lua
+++ .config/nvim/lua/amal/core/settings.lua
@@ -20,13 +20,12 @@ vim.opt.showtabline = 0
 vim.o.confirm = false
 
 -- clipboard
-vim.cmd("set clipboard+=unnamedplus")
+vim.opt.clipboard = "unnamedplus"
 -- vim.cmd("set paste") -- deprecated (disables a bunch of features)
 
 -- status bar
-vim.cmd("set noshowmode")
-vim.cmd("set conceallevel=0")
-vim.cmd("set cmdheight=0")
+vim.opt.conceallevel = 0
+vim.opt.cmdheight = 0
 
 -- split screen config (only splits to right and bottom sides)
 vim.opt.splitright = true
@@ -44,30 +43,28 @@ vim.opt.spelllang = { "en_us" }
 
 -- misc config
 vim.o.guifont = "JetBrainsMono Nerd Font:h11" -- sets the default font and height
-vim.g.mapleader = " " -- maps the leader key
-vim.g.maplocalleader = "\\" -- local leader key config
 vim.opt.autoread = true -- updates vim after file update outside
 -- vim.opt.statuscolumn = "%s %l %r " -- relative line numbers
-vim.cmd("set number") -- sets line numbers
-vim.cmd("set relativenumber") -- sets relative line numbers
-vim.cmd("set nowrap") -- turns off word wrap
-vim.cmd("set fileformat=unix") -- turns file format to unix style
+vim.opt.number = true -- sets line numbers
+vim.opt.relativenumber = true -- sets relative line numbers
+vim.opt.wrap = false -- turns off word wrap
+vim.opt.fileformat = "unix" -- turns file format to unix style
 vim.opt.showmode = false -- disables the ~ symbol (end of buffer indicator)
 vim.o.termguicolors = true -- sets proper terminal colors
 vim.o.completeopt = "menuone,noselect" -- have a better completion experience
 vim.opt.cursorline = true -- sets the cursor line
 vim.opt.ignorecase = true -- ignore case in search patterns
-vim.opt.numberwidth = 4 -- set number column width to 2 {default 4}
+vim.opt.smartcase = true  -- makes search case-sensitive when you use uppercase
+vim.opt.numberwidth = 4 -- set number column width to 4 {default 4}
 vim.opt.sidescrolloff = 8 -- Makes sure there are always eight lines of context
 vim.opt.fillchars = { eob = " " } -- change the character at the end of buffer
+vim.opt.signcolumn = "yes" -- fixes sign column jump when diagnostics appear
 
 -- timeout and update config
 vim.opt.timeoutlen = 1000 -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.updatetime = 100 -- faster completion (4000ms default)
 
 -- terminal config
-vim.cmd([[let &shell = '"C:/Program Files/Git/bin/bash.exe"']])
-vim.cmd([[let &shellcmdflag = '-s']])
 vim.g.floaterm_width = 0.7
 vim.g.floaterm_height = 0.8
 vim.g.floaterm_position = "center"
