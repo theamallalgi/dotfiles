5e313797a6cfbc141f8f761ea9e1147a953d6fb0
Amal Lalgi <amallalgi2487@gmail.com>
2026-05-16 12:02:55 +0530

    fix: fix support for lua code

--- .config/nvim/lua/amal/plugins/lsp/servers.lua
+++ .config/nvim/lua/amal/plugins/lsp/servers.lua
@@ -27,4 +27,17 @@ return {
 			},
 		},
 	},
+	lua_ls = {
+		settings = {
+			Lua = {
+				runtime = { version = "LuaJIT" },
+				diagnostics = { globals = { "vim" } },
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+					checkThirdParty = false,
+				},
+				telemetry = { enable = false },
+			},
+		},
+	},
 }
