7ead7af3ca92a8a853c6d093aed6162375958c42
Amal Lalgi <amallalgi2487@gmail.com>
2026-05-16 12:02:31 +0530

    feat: add extras to help with web-dev

---
+++ .config/nvim/lua/amal/plugins/extras.lua
@@ -0,0 +1,5 @@
+return {
+  { "pmizio/typescript-tools.nvim", dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" } },
+  { "windwp/nvim-ts-autotag" },
+  { "luckasRanarison/tailwind-tools.nvim" }
+}
