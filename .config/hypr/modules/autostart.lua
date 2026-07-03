fb38d1d3f4857d659ef985206c041b1d5cd30e0e
Amal Lalgi <amallalgi2487@gmail.com>
2026-07-03 21:20:22 +0530

    chore: add gnome-keyring-daemon to autostart list

--- .config/hypr/modules/autostart.lua
+++ .config/hypr/modules/autostart.lua
@@ -6,6 +6,8 @@ hl.on("hyprland.start", function()
     hl.exec_cmd("systemctl --user start xdg-desktop-portal-hyprland")
     hl.exec_cmd("systemctl --user start xdg-desktop-portal")
     hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+    hl.exec_cmd("gnome-keyring-daemon --start --components=secrets,pkcs11,ssh")
+    hl.exec_cmd("/usr/lib/gvfsd")
 
     -- display and wallpaper
     hl.exec_cmd("awww-daemon")
