deac349fba1654205336bd904eebeff710a25cb2
Amal Lalgi <amallalgi2487@gmail.com>
2026-08-24 00:38:07 +0530

    feat(nvim/plugin): add `theprimeagen/harpoon`

--- .config/nvim/lua/amal/plugins/harpoon.lua
+++ .config/nvim/lua/amal/plugins/harpoon.lua
@@ -1,32 +1,55 @@
 return {
-  "ThePrimeagen/harpoon",
-  branch = "harpoon2",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local harpoon = require("harpoon")
-    harpoon:setup()
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local harpoon = require("harpoon")
+		harpoon:setup()
 
-    local silent = { silent = true }
-    local set = vim.keymap.set
+		local silent = { silent = true }
+		local set = vim.keymap.set
 
-    -- mark / add file
-    set("n", "<C-o>", function() harpoon:list():add() end, silent)
+		-- mark / add file
+		set("n", "<C-t>", function()
+			harpoon:list():add()
+		end, silent)
 
-    -- toggle quick menu
-    set("n", "<C-p>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, silent)
+		-- toggle quick menu
+		set("n", "<C-y>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, silent)
 
-    -- navigate to harpooned files
-    -- set("n", "<C-e>", function() harpoon:list():select(1) end, silent)
-    -- set("n", "<C-r>", function() harpoon:list():select(2) end, silent)
-    -- set("n", "<C-t>", function() harpoon:list():select(3) end, silent)
-    -- set("n", "<C-y>", function() harpoon:list():select(4) end, silent)
-    set("n", "<C-h>", function() harpoon:list():select(1) end, silent)
-    set("n", "<C-j>", function() harpoon:list():select(2) end, silent)
-    set("n", "<C-k>", function() harpoon:list():select(3) end, silent)
-    set("n", "<C-l>", function() harpoon:list():select(4) end, silent)
+		-- navigate to harpooned files
+		-- set("n", "<C-e>", function() harpoon:list():select(1) end, silent)
+		-- set("n", "<C-r>", function() harpoon:list():select(2) end, silent)
+		-- set("n", "<C-t>", function() harpoon:list():select(3) end, silent)
+		-- set("n", "<C-y>", function() harpoon:list():select(4) end, silent)
+		set("n", "<C-h>", function()
+			harpoon:list():select(1)
+		end, silent)
+		set("n", "<C-j>", function()
+			harpoon:list():select(2)
+		end, silent)
+		set("n", "<C-k>", function()
+			harpoon:list():select(3)
+		end, silent)
+		set("n", "<C-l>", function()
+			harpoon:list():select(4)
+		end, silent)
 
-    -- next / prev in list
-    set("n", "<C-S-P>", function() harpoon:list():prev() end, silent)
-    set("n", "<C-S-N>", function() harpoon:list():next() end, silent)
-  end,
+		-- navigate to harpooned files (leader + number)
+		for i = 1, 9 do
+			set("n", "<leader>" .. i, function()
+				harpoon:list():select(i)
+			end, { silent = true, desc = "Harpoon file " .. i })
+		end
+
+		-- next / prev in list
+		set("n", "<C-S-P>", function()
+			harpoon:list():prev()
+		end, silent)
+		set("n", "<C-S-N>", function()
+			harpoon:list():next()
+		end, silent)
+	end,
 }
