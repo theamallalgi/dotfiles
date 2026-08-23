8c795eff0704b41095f3e8c8c6e7b27be9f26e39
Amal Lalgi <amallalgi2487@gmail.com>
2026-08-24 00:32:47 +0530

    test: for zitchdog

--- .config/nvim/lua/amal/core/uiconfig.lua
+++ .config/nvim/lua/amal/core/uiconfig.lua
@@ -1,14 +1,7 @@
 -- uiconfig.lua
-
--- local red = "#331b17" -- zitchdog (inverted)
--- local black = "#e4384c" -- zitchdog (inverted)
-local red = "#e4384c" -- zitchdog
-local black = "#331b17" -- zitchdog
--- local red = "#ff6767" -- default
--- local black = "#1a1b26" -- default
+local palette = require("colors")
 
 -- highlight text on yank
-vim.api.nvim_set_hl(0, "IncSearch", { bg = red, fg = black })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
 		vim.highlight.on_yank({
@@ -18,37 +11,25 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
--- gitdiff (foreground colors fix)
-vim.api.nvim_set_hl(0, "GitSignsAddLn", { bg = "#091511", fg = "NONE", })
-vim.api.nvim_set_hl(0, "GitSignsChangeLn", { bg = "#121429", fg = "NONE", })
-vim.api.nvim_set_hl(0, "GitSignsDeleteLn", { bg = "#240d12", fg = "NONE", })
-vim.api.nvim_set_hl(0, "GitSignsAddInline", { bg = "#091511", fg = "NONE", })
-vim.api.nvim_set_hl(0, "GitSignsChangeInline", { bg = "#121429", fg = "NONE", })
-vim.api.nvim_set_hl(0, "GitSignsDeleteInline", { bg = "#240d12", fg = "NONE", })
-
--- undotree
-vim.api.nvim_set_hl(0, "UndotreeDiffAdd",    { bg = "#00210F", fg = "NONE" })
-vim.api.nvim_set_hl(0, "UndotreeDiffChange", { bg = "#0D0F1F", fg = "NONE" })
-vim.api.nvim_set_hl(0, "UndotreeDiffDelete", { bg = "#210500", fg = "NONE" })
--- vim.api.nvim_set_hl(0, "UndotreeDiffAdd",    { bg = "#091511", fg = "NONE" })
--- vim.api.nvim_set_hl(0, "UndotreeDiffChange", { bg = "#0d1e18", fg = "NONE" })
--- vim.api.nvim_set_hl(0, "UndotreeDiffDelete", { bg = "#240d12", fg = "NONE" })
+vim.api.nvim_set_hl(0, "Search", { bg = "#331b17", fg = "#e39069" }) -- search highlights
+-- vim.api.nvim_set_hl(0, "MatchParen", { bg = "#e39069", fg = "#331b17", bold = true, })
 
 vim.api.nvim_set_hl(0, "BlinkCmpMenu", { bg = "#191324" })
 vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { fg = "#191324", bg = "#191324" })
 vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { bg = "#201638" })
 vim.api.nvim_set_hl(0, "BlinkCmpDoc", { bg = "#191324" })
-vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", { fg = "#191324", bg = "#191324", })
+vim.api.nvim_set_hl(0, "BlinkCmpDocBorder", { fg = "#191324", bg = "#191324" })
 
-vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#abb2bf", bg = "#0D0910", })
-vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#2c1e36", bg = "NONE", })
-vim.api.nvim_set_hl(0, "FloatTitle", { fg = "#abb2bf", bg = "#0D0910", bold = true, })
-vim.api.nvim_set_hl(0, "FloatFooter", { fg = "#abb2bf", bg = "#0D0910", })
-vim.api.nvim_set_hl(0, "LspFloatWinNormal", { link = "NormalFloat", })
-vim.api.nvim_set_hl(0, "LspFloatWinBorder", { link = "FloatBorder", })
+vim.api.nvim_set_hl(0, "NormalFloat", { fg = "#abb2bf", bg = "#0D0910" })
+vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#2c1e36", bg = "NONE" })
+-- vim.api.nvim_set_hl(0, "FloatTitle", { fg = "#abb2bf", bg = "#0D0910", bold = true })
+vim.api.nvim_set_hl(0, "FloatTitle", { fg = "#c0c9c3", bg = "#051177", bold = true })
+vim.api.nvim_set_hl(0, "FloatFooter", { fg = "#abb2bf", bg = "#0D0910" })
+vim.api.nvim_set_hl(0, "LspFloatWinNormal", { link = "NormalFloat" })
+vim.api.nvim_set_hl(0, "LspFloatWinBorder", { link = "FloatBorder" })
 
-vim.api.nvim_set_hl(0, "LazyNormal", { fg = "#abb2bf", bg = "#0D0910", })
-vim.api.nvim_set_hl(0, "LazyBorder", { fg = "#140D19", bg = "#140D19", })
-vim.api.nvim_set_hl(0, "LazyButton", { fg = "#abb2bf", bg = "#140D19", })
-vim.api.nvim_set_hl(0, "LazyButtonActive", { fg = "#c0c9c3", bg = "#051177", bold = true, })
-vim.api.nvim_set_hl(0, "LazyH1", { fg = "#c0c9c3", bg = "#051177", bold = true, })
+vim.api.nvim_set_hl(0, "LazyNormal", { fg = "#abb2bf", bg = "#0D0910" })
+vim.api.nvim_set_hl(0, "LazyBorder", { fg = "#140D19", bg = "#140D19" })
+vim.api.nvim_set_hl(0, "LazyButton", { fg = "#abb2bf", bg = "#140D19" })
+vim.api.nvim_set_hl(0, "LazyButtonActive", { fg = "#c0c9c3", bg = "#051177", bold = true })
+vim.api.nvim_set_hl(0, "LazyH1", { fg = "#c0c9c3", bg = "#051177", bold = true })
