2dc84f41c9d3da23327f3f8bc367d017b20cb193
Amal Lalgi <amallalgi2487@gmail.com>
2026-09-06 22:48:07 +0530

    chore(nvim): add fallback colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,7 @@
 
 vim.g.mapleader = " " -- maps the leader key
 vim.g.maplocalleader = "\\" -- local leader key config
+vim.cmd.colorscheme("catppuccin") -- fallback colors
 
 -- disable broken bundled vim treesitter queries on nvim 0.12+
 vim.treesitter.query.set("vim", "highlights", "")
