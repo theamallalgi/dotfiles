b32a48e48d6d6d4851dff55677fd3c982cafe363
Amal Lalgi <amallalgi2487@gmail.com>
2026-09-06 22:50:27 +0530

    chore: add meaningless tweaks to ui2 setup

--- .config/nvim/lua/amal/core/config/ui2.lua
+++ .config/nvim/lua/amal/core/config/ui2.lua
@@ -11,19 +11,13 @@ if false then
 			---@type string|table<string, 'cmd'|'msg'|'pager'> Default message target
 			---or table mapping |ui-messages| kinds and triggers to a target.
 			targets = "cmd",
-			cmd = { -- Options related to messages in the cmdline window.
-				height = 0.5, -- Maximum height while expanded for messages beyond 'cmdheight'.
+			cmd = { height = 0.5 },
+			dialog = { height = 0.5 },
+			msg = {
+				height = 0.5,
+				timeout = 4000,
 			},
-			dialog = { -- Options related to dialog window.
-				height = 0.5, -- Maximum height.
-			},
-			msg = { -- Options related to msg window.
-				height = 0.5, -- Maximum height.
-				timeout = 4000, -- Time a message is visible in the message window.
-			},
-			pager = { -- Options related to message window.
-				height = 1, -- Maximum height.
-			},
+			pager = { height = 1 },
 		},
 	})
 end
