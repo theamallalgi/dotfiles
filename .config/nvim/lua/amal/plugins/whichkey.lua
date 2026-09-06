02dde5a5f9dd05b57a6ce1db2fda44a38f4f9d2e
Amal Lalgi <amallalgi2487@gmail.com>
2026-09-06 22:56:13 +0530

    chore: change a few glyphs

--- .config/nvim/lua/amal/plugins/whichkey.lua
+++ .config/nvim/lua/amal/plugins/whichkey.lua
@@ -3,12 +3,13 @@ return {
 	event = "VeryLazy",
 	opts = {
 		preset = "helix",
-		delay = 500,
+		delay = 250,
+		-- sort = { "alphanum", "local", "order", "mod", "group" },
 		icons = {
 			rules = false,
-			breadcrumb = " ", -- symbol used in the command line area that shows your active key combo
-			separator = "󱦰  ", -- symbol used between a key and it's label
-			group = "󰹍 ", -- symbol prepended to a group
+			breadcrumb = "󰅂 ", -- symbol used in the command line area that shows your active key combo
+			separator = "󰁔  ", -- symbol used between a key and it's label
+			group = "󰴮 ", -- symbol prepended to a group
 		},
 		plugins = {
 			spelling = {
@@ -16,11 +17,10 @@ return {
 			},
 		},
 		win = {
-			height = {
-				max = math.huge,
-			},
+			height = { max = math.huge },
+			padding = { 1, 2 },
 		},
-    spec = require("amal.plugins.config.whichkey-specs"),
+		spec = require("amal.plugins.config.whichkey-specs"),
 	},
 	keys = {
 		{
