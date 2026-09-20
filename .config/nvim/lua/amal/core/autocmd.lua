d31a9a8d59385b6c2630653142915438c17cab9e
Amal Lalgi <amallalgi2487@gmail.com>
2026-09-21 01:08:48 +0530

    chore(nvim): remove centerize current line on load

--- .config/nvim/lua/amal/core/autocmd.lua
+++ .config/nvim/lua/amal/core/autocmd.lua
@@ -16,7 +16,7 @@ api.nvim_create_autocmd("BufReadPost", {
 		local lcount = vim.api.nvim_buf_line_count(0)
 		if mark[1] > 0 and mark[1] <= lcount then
 			pcall(vim.api.nvim_win_set_cursor, 0, mark)
-			vim.cmd("normal! zz")
+			-- vim.cmd("normal! zz")
 		end
 	end,
 })
